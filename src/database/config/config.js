452ec147bb5bbbbd21810a18f6806a06ecd87207
=Alejo Tarrio <alejo.david.tarrio@gmail.com>
2023-10-16 20:30:06 -0300

    Proyecto completo

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'Ale123',
+    database: 'library',
+    host: 'localhost',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'root',
+    password: 'Ale123',
+    database: 'database_test',
+    host: 'localhost',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'root',
+    password: 'Ale123',
+    database: 'database_production',
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+};
